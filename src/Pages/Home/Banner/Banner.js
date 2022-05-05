afe5942ec68e8b8f03636c93eb293e53dc311360
mehedifoysal <mehedifoysal8@gmail.com>
2022-05-05 22:27:18 +0600

    banner component

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -10,8 +10,8 @@ const Banner = () => {
                     <Row>
                         <Col lg='6'>
                             <div className="banner-content">
-                                <h1 className="">Best Inventory<br/> For Your Business<br/> To Grow</h1>
-                                <p className=""><b>ZOHO</b> is an Inventory Management System that
+                                <h1 className=""><span>Best Inventory</span><br/> For Your Business<br/> To Grow</h1>
+                                <p className=""><b>Zoho</b> is an Inventory Management System that
                                     allows you to manage your inventory in a more efficient way and maximize on the
                                     business potentials</p>
                                 <a href="" className="btn-default">View Demo</a>
