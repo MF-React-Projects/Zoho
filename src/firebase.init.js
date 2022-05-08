aac484adb4cf88ce15ecc469885666af87a2a2dd
mehedifoysal <mehedifoysal8@gmail.com>
2022-05-08 08:41:07 +0600

    firebase env setup

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDQypTm6pCY_lQKF9ignhmhx4VURcqoEIM",
-    authDomain: "zoho-26c66.firebaseapp.com",
-    projectId: "zoho-26c66",
-    storageBucket: "zoho-26c66.appspot.com",
-    messagingSenderId: "461988127226",
-    appId: "1:461988127226:web:cd94d8da9a27fb8720c443"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
